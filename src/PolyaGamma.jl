678a24f36cb601a1b0f6a99f6c02298028a7c6ab
Michael J. Curry <curry@cs.umd.edu>
2017-11-20 03:16:06 -0500

    mean of polya gamma distribution

--- src/PolyaGamma.jl
+++ src/PolyaGamma.jl
@@ -1,5 +1,14 @@
 module PolyaGamma
+using Distributions
 
+struct PolyaGammaDistribution{T<:Real} <: ContinuousUnivariateDistribution
+    b::T
+    c::T
+end
+
+function mean(d::PolyaGammaDistribution)
+    (d.b / (2.0*d.c)) * tanh(d.c / 2.0)
+end
 # package code goes here
-
+export PolyaGammaDistribution, mean
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using PolyaGamma
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@test PolyaGamma.mean(PolyaGamma.PolyaGammaDistribution(2.0, 3.0)) ≈ 0.3017160845482888
